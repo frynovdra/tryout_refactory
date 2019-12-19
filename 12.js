66fad1ff628c7a27f886c490d3c92df415b6e48e
frynovdra <59051375+frynovdra@users.noreply.github.com>
2019-12-19 20:43:32 +0700

    Update 12.js

--- 12.js
+++ 12.js
@@ -1,10 +1,9 @@
 const reverseString = (str) =>  {
     var newString = "";
     for (var i = str.length - 1; i >= 0; i--) { 
-        newString += str[i];
+        newString += str[i]; //Loop mundur untuk menyisipkan string lama kedalam string yang baru
     }
-    console.log(str + newString)
+    console.log(str + newString) //"Concatenate" string lama dan baru
 }
-
-reverseString('hello');
-reverseString('--oO');
+reverseString('hello'); //Output : helloolleh
+reverseString('--oO'); //Output : --oOOo--
