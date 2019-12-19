9c183d0b573db02b24f991fdf1f38b7618e5567f
frynovdra <59051375+frynovdra@users.noreply.github.com>
2019-12-19 20:26:15 +0700

    Update 02.js

--- 02.js
+++ 02.js
@@ -1,12 +1,23 @@
-
-const check = (num) => {
-  if (num % 2 == 0) {
-    console.log("Input : " + num);
-    console.log("Output : even");
-  } else {
-    console.log("Input : " + num);
-    console.log("Output : odd");
-  }
+const grade = prompt("Masukan nilai : " , 0);
+                     
+switch (true) {
+  case grade >= 90:
+    console.log("Input : " + grade);
+    console.log("Output : A");
+    break;
+  case grade >= 80:
+    console.log("Input : " + grade);
+    console.log("Output : B");
+    break;
+  case grade >= 70:
+    console.log("Input : " + grade);
+    console.log("Output : C");
+    break;
+  case grade >= 60:
+    console.log("Input : " + grade);
+    console.log("Output : D");
+    break;
+  default:
+    console.log("Input : " + grade);
+    console.log("Output : E");
 }
-check(90);
-check(35);
