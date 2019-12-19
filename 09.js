b6a7948babc58708ab3f5dc69054c7eb451c1906
frynovdra <59051375+frynovdra@users.noreply.github.com>
2019-12-19 22:31:54 +0700

    Update and rename 09.js to 09.html

--- 09.js
+++
@@ -1,7 +0,0 @@
-const kata = ['Saya','ingin','makan','nasi','goreng'];
-const kataBaru = [];
-
-for(let i = kata.length; i >= 0; i--){
-    kataBaru.push(kata[i]);
-}
-console.log(kataBaru.slice(0,6).join(' '));
