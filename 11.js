89e6aa944e4dae706905cb3465e28bee9af91b48
frynovdra <59051375+frynovdra@users.noreply.github.com>
2019-12-19 20:45:59 +0700

    Update 11.js

--- 11.js
+++ 11.js
@@ -1,2 +1,3 @@
 const fruits = ['Jeruk', 'Pepaya', 'Jambu', 'Anggur', 'Melon'];
-console.log(fruits.slice(0,2) + ',' + fruits.slice(3,5));
+console.log(fruits.slice(0,2) + ',' + fruits.slice(3,5)); /*Slice semua elemen kecuali melon, kemudian elemen
+                                                           yang terpotong di log dan digabungkan kembali */
