9dfb62250dc011609cd079a8d991e50319ed86fe
frynovdra <59051375+frynovdra@users.noreply.github.com>
2019-12-19 20:32:04 +0700

    Update 03.js

--- 03.js
+++ 03.js
@@ -1,11 +1,11 @@
 const check = (num) => {
-  if (num % 2 == 0) {
+  if (num % 2 == 0) { //Cek genap tidaknya bilangan apabila tidak ada yang tersisa setelah habis dibagi 2
     console.log("Input : " + num);
-    console.log("Output : even");
-  } else {
+    console.log("Output : Genap");
+  } else { //Jika masih ada sisa bilangan yang keluar adalah ganjil
     console.log("Input : " + num);
-    console.log("Output : odd");
+    console.log("Output : Ganjil");
   }
 }
-check(90); //Output : even
-check(35); //Output : odd
+check(90); //Output : Genap 
+check(35); //Output : Ganjil
