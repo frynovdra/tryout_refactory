3ac5504a799e7eb5bb58308d04b9cf1595ca979c
frynovdra <59051375+frynovdra@users.noreply.github.com>
2019-12-19 22:16:59 +0700

    Update and rename 01.js to 01.html

--- 01.js
+++
@@ -1,7 +0,0 @@
-var karakter = prompt("Input : ", "Masukan nilai...");
-
-if (karakter != null){
-  console.log("Output : " + karakter.length);
-} else {
-  console.console.log("Masukan input!");
-}
