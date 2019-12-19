bfccf9ee98a0e16315a423d4c3635d91f75d82e4
frynovdra <59051375+frynovdra@users.noreply.github.com>
2019-12-19 20:38:24 +0700

    Update 05.js

--- 05.js
+++ 05.js
@@ -1,12 +1,19 @@
+const age = prompt('Masukkan usia : ');
 
-    const age = prompt('Masukkan usia : ');
-
-    if(age >= 21){
-        console.log('Dewasa.');
-    } else if(age age >= 13){
-        console.log('Remaja.');
-    } else if(age age >= 9){
-        console.log('Bimbingan orang tua.');
-    } else if(age age < 9){
-        console.log('Semua Umur.');
-    }
+switch(true){
+    case age >= 21:
+        console.log('Input : ' + age);
+        console.log('Output : Dewasa.');
+        break;
+    case age >= 13:
+        console.log('Input : ' + age);
+        console.log('Output : Remaja.');
+        break;
+    case age >= 9:
+        console.log('Input : ' + age);
+        console.log('Output : Bimbingan Orang Tua.');
+        break;
+    default:
+        console.log('Input : ' + age);
+        console.log('Output : Semua Umur.');
+}
